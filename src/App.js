d0e373b8045e797839293648aa2a097367357643
JosueBuck <buck.josue@gmx.de>
2022-01-20 02:36:51 +0100

    Add Recipe Widget Component
    - Create Recipe Widget Component
    - Style Recipe Widget Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import LoginPage from "./components/authentication/Login/LoginPage";
 import RegisterPage from './components/authentication/Register/RegisterPage';
 import ForgotPasswordPage from "./components/authentication/ForgotPassword/ForgotPasswordPage";
 import './css/style.css';
+import RecipeWidgetComponent from "./components/recipe/RecipeWidgetComponent";
 
 function App() {
 
---
+++ src/components/recipe/RecipeWidgetComponent.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { MdFastfood } from "react-icons/md";
+import { FiClock } from "react-icons/fi";
+
+const RecipeWidgetComponent = ({recipePictureUrl, recipeName, recipeGarnishName, cookingTime, recipeType}) => {
+
+    const widgetRecipePictureStyling = {
+        backgroundImage: 'url(' + recipePictureUrl + ')',
+        backgroundSize: 'contain',
+    }
+
+    return <div className='recipeWidgetContainer'>
+        <div style={widgetRecipePictureStyling} className='recipeWidgetPicture'></div>
+        <div className='recipeWidgetInfoContainer'>
+            <div className='recipeWidgetNameContainer'>
+                <p className='recipeName'>{recipeName}</p>
+                <p className='recipeGarnishName'>{recipeGarnishName}</p>
+            </div>
+            <div className='recipeWidgetInfoIconsContainer'>
+                <div className='cookingTimeContainer'>
+                    <FiClock className='cookingTimeIcon' />
+                    <FiClock className={`cookingTimeIcon ${cookingTime > 9 ? '' : 'passive'}`}/>
+                    <FiClock className={`cookingTimeIcon ${cookingTime > 29 ? '' : 'passive'}`} />
+                </div>
+                <div className='recipeTypeContainer'>
+                    <MdFastfood className='recipeTypeIcon' />
+                </div>
+            </div>
+        </div>
+    </div>;
+};
+
+export default RecipeWidgetComponent;
